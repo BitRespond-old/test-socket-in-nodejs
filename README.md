464bc4a926fed98b06413b7f285c082949d38f73
AxeemHaider <axeemhaider@gmail.com>
2021-11-27 18:33:01 +0500

    some problems found

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 # test-socket-in-nodejs
+
 Test socket in nodejs
 
 Initial code is taken from [Get stated | Socket.io](https://socket.io/get-started/chat)
+
+# Problems
+
+1. Nested socket listener is `whatsapp:login`
+2. On Auth return user is logged in
+3. init `initWebSocket` and `initWhatsappServices` after Auth successfully connected
+   probably move into `client.on("authenticated")`
--- index.js
+++ index.js
@@ -2,15 +2,26 @@ const express = require("express");
 const app = express();
 const http = require("http");
 const server = http.createServer(app);
+const { Client } = require("whatsapp-web.js");
 const { Server } = require("socket.io");
 const io = new Server(server);
+const client = new Client({ puppeteer: { headless: false } });
+client.initialize();
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
 io.on("connection", (socket) => {
-  console.log("a user connected");
+  console.log("a user connected change 1");
+
+  socket.on("login", () => {
+    console.log("login changes 4");
+
+    client.on("qr", (qr) => {
+      console.log("qr changes 1");
+    });
+  });
 });
 
 server.listen(3000, () => {
