a1d34b44794a238259854021b67867183e7a66dc
AxeemHaider <axeemhaider@gmail.com>
2021-11-28 15:32:04 +0500

    update

--- autoprocess.js
+++ autoprocess.js
@@ -1,2 +1,2 @@
-console.log("auto process change 3");
+console.log("auto process change 5");
 console.log(process.env.devMode);
--- browser.js
+++ browser.js
@@ -3,7 +3,7 @@ const puppeteer = require("puppeteer");
 (async () => {
   console.log("Starting browser");
 
-  const browser = await puppeteer.launch({ headless: true });
+  const browser = await puppeteer.launch({ headless: false });
   const wsEndpoint = browser.wsEndpoint();
 
   console.log("Browser start on");
